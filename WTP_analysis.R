f6e13a5999525f1b2acca06c63cea6805bd67d9b
Rachel Carlson <Rachel.R.Carl@gmail.com>
2022-07-01 08:49:35 -0700

    Revisions

--- WTP_analysis.R
+++ WTP_analysis.R
@@ -17,19 +17,26 @@ library(lmtest)
 library(psych)
 library(lme4)
 library(ResourceSelection)
+library(bestglm)
 
 ##### Read in data
-wtp <- read.csv("/Users/rachelcarlson/Documents/Research/Coral_Insurance/Data/business_wtp.csv", stringsAsFactors = TRUE)
+wtp <- read.csv("/Users/rachelcarlson/Documents/Research/Coral_Insurance/Data/business_wtp.csv")
 
 ##### Clean data
 # Remove geospatial markers (not needed) and any other uneeded columns
 wtp <- wtp %>% select(-c(address, lat_land, lon_land, lat_shore, lon_shore, loc_sea, lat_sea, lon_sea)) # Remove geospatial
 wtp <- wtp %>% select(-email)
 
-# Some irregularities in "island"
+# Some irregularities in "island" and other variables
 unique(wtp$island)
 wtp$island <- ifelse(wtp$island == "Hawaii\tNorth Kona","Hawaii", wtp$island) # Fix mistake
 wtp$island <- ifelse(wtp$island == "", NA, wtp$island) # Translate blank to NA
+wtp$prox <- ifelse(wtp$prox == "Close to (<1 mile)", "Close", wtp$prox)
+wtp$dist <- ifelse(wtp$dist == "in person " | wtp$dist == "in", "in person", wtp$dist)
+wtp$gender <- ifelse(wtp$gender == "", "Other", wtp$gender)
+wtp$industry1 <- ifelse(wtp$industry1 == "Recreational surface", "Recreation surface", wtp$industry1)
+wtp$CC <- ifelse(wtp$CC == 555, 3, wtp$CC) # Don't want to exclude "unsure" respondents from regression but this high value may skew effect size
+
 
 ##### Summary statistics
 
@@ -89,7 +96,37 @@ glm.pred <- rep(0, 174)
 glm.pred[glm.probs > 0.5] = 1
 table(glm.pred, wtp_m1$BID5)
 
-# Perform best subset selection
+##### Perform best subset selection
+# Redefine dataset to include only the variables I am testing
+wtp_ms <- wtp[,c("dist", "res", "identity", "age", "gender", "island", "CC", "industry1", "prox", "size_rev", "tenure", "econ_1", "econ_2.1","pro_nat1","pro_soc", "BID1.25", "BID2.5", "BID5")]
+wtp_ms <- na.omit(wtp_ms)
+wtp_ms[sapply(wtp_ms, is.character)] <- lapply(wtp_ms[sapply(wtp_ms, is.character)], 
+                                       as.factor)
+# Rename outcome as "y" (required by "bestglm()" function)
+wtp_ms_1.25 <- wtp_ms %>% select(-c(BID2.5, BID5)) %>% rename(y = BID1.25)
+wtp_ms_2.5 <- wtp_ms %>% select(-c(BID1.25, BID5)) %>% rename(y = BID2.5)
+wtp_ms_5 <- wtp_ms %>% select(-c(BID1.25, BID2.5)) %>% rename(y = BID5)
+
+best.logit <- bestglm(wtp_ms_1.25,
+                      IC = "AIC",                 # Information criteria for
+                      family=binomial,
+                      method = "exhaustive")
+summary(best.logit$BestModel) # AIC
+summary(best.logit.1$BestModel) # BIC
+
+best.logit2 <- bestglm(wtp_ms_2.5,
+                      IC = "AIC",                 # Information criteria for
+                      family=binomial,
+                      method = "exhaustive")
+summary(best.logit2$BestModel)
+summary(best.logit2.1$BestModel)
+
+best.logit3 <- bestglm(wtp_ms_5,
+                      IC = "AIC",                 # Information criteria for
+                      family=binomial,
+                      method = "exhaustive")
+summary(best.logit3$BestModel)
+summary(best.logit3.1$BestModel)
 
 
 # Use multinomial logistic regression instead, for fun (not used in final analysis)
